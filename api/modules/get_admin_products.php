183a4bf783f4c8fdbf3010abbd8335000d603ff3
Yeminfu <yeminfu4319@gmail.com>
2023-04-25 17:01:18 +1000

    add meta

--- api/modules/get_admin_products.php
+++ api/modules/get_admin_products.php
@@ -3,22 +3,9 @@
 
 $filterBy = [];
 
-
-// echo json_encode([
-//     'success' => true,
-//     'data' => $values_from_post_json
-// ]);
-// exit();
-
-// if()
 if (isset($values_from_post_json['category'])) {
     $category_name = $values_from_post_json['category'];
     $filterBy[] = "category IN (SELECT id FROM categories WHERE category_name = '$category_name')";
-    // echo json_encode([
-    //     'success' => false,
-    //     'error' => 'Нет категории товара'
-    // ]);
-    // exit();
 }
 
 
@@ -43,7 +30,6 @@ if (isset($values_from_post_json['params'])) {
     }
 }
 
-
 $where_string = count($filterBy) > 0 ? " WHERE " . implode(" AND ", $filterBy) : "";
 
 $qs = "SELECT $columns from products $where_string";
@@ -57,36 +43,20 @@ if (!is_array($products)) {
     exit();
 }
 
-array_walk($products, function (&$product) {
-    global $mysqli;
-    $product_category = $product['category'];
-    $product_id = $product['id'];
+$total = 0;
 
-    $attributes = $mysqli->query("SELECT id, attribute_name FROM attributes WHERE category = '$product_category'")->fetch_all(MYSQLI_ASSOC);
-    foreach ($attributes as $key => $attribute) {
-        $attribute_id = $attribute['id'];
-        $attribute_value = $mysqli->query("SELECT value_name FROM attributes_values WHERE id IN
-            (SELECT attribute FROM attr_prod_relation WHERE product = $product_id AND attribute = $attribute_id)
-        ")->fetch_assoc();
-        $attributes[$key]['value'] = $attribute_value['value_name'] ?? "-";
-    }
-    $product['attributes'] = ($attributes);
+$per_page = $values_from_post_json['per_page'];
 
-    $images = $mysqli->query("SELECT name FROM `products_media` WHERE product_id = $product_id AND type='image_full'")->fetch_all(MYSQLI_ASSOC);
-    $product['images'] = array_map(function ($image) {
-        global $config;
-        return $config['homeurl'] . "/images/" . $image['name'];
-    }, $images);
+$total = $mysqli->query("SELECT COUNT(*) as total from products $where_string")->fetch_assoc()['total'];
+$pages = ceil($total / $per_page);
 
-    $product_status_id = $product['stock_status'];
-    $stock_status = $mysqli->query("SELECT * FROM stock_statuses WHERE id = $product_status_id")->fetch_assoc();
-    $product['stock_status'] = $stock_status['status_name'];
-});
 
 echo json_encode([
     'success' => true,
     'data' => [
-        'products' => $products
+        'products' => $products,
+        'total' => intval($total),
+        'pages' => $pages,
     ],
 ]);
 die();
