af7143a040ad319b1ef361e105955435f04e4b8d
Yeminfu <yeminfu4319@gmail.com>
2023-05-09 17:24:44 +1000

    create attributes values creator

--- index.php
+++ index.php
@@ -811,4 +811,8 @@ if (isset($uri[1]) && $uri[1] == 'api') {
         require __DIR__ . "/api/modules/admin-attributes/delete-attribute.php";
         exit();
     }
+    if ((isset($uri[2]) && $uri[2] == 'create-attribute-value')) {
+        require __DIR__ . "/api/modules/admin-attributes/create-attribute-value.php";
+        exit();
+    }
 }
