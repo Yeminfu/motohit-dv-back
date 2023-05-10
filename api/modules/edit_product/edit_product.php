8f761e00f2666c1e738732d8364d0007de508707
Yeminfu <yeminfu4319@gmail.com>
2023-05-10 11:14:13 +1000

    fix attributes in edit product

--- api/modules/edit_product/edit_product.php
+++ api/modules/edit_product/edit_product.php
@@ -1,7 +1,7 @@
 <?php
 
 $product_id = $_POST['product_id'];
-
+$log = [];
 
 if (isset($_POST['downloadedImages'])) {
     $downloadedImages = $_POST['downloadedImages'];
@@ -15,11 +15,35 @@ if (isset($_POST['downloadedImages'])) {
         $imagesInDB,
         $downloadedImages,
     );
- 
+
     foreach ($diff as $key => $image_id) {
         $image_name = $mysqli->query("SELECT name FROM products_media WHERE id = $image_id")->fetch_assoc();
         $deleteRes = $mysqli->query("DELETE FROM products_media WHERE id = $image_id");
         $path_to_file = $config['uploaddir'] . "/" . $image_name['name'];
+    }
+}
+
+
+if (isset($_POST['attributes'])) {
+    $log[] = ['Прислали атрибуты'];
+    foreach ($_POST['attributes'] as $attribute_id => $attribute_value_id) {
+        $qs = "SELECT * FROM attr_prod_relation WHERE attribute = $attribute_id AND product = $product_id";
+        $log[] = ['заправшиваем наличие атрибута у товара', $qs];
+        $res = $mysqli->query($qs)->fetch_assoc();
+
+        if ($res) {
+            $log[] = ['Такой атрибут у товара есть', $res];
+            $qs_update_relation = "UPDATE attr_prod_relation SET attribute_value = $attribute_value_id WHERE id = " . $res['id'];
+            $res2 = $mysqli->query($qs_update_relation);
+            if ($res2) {
+                $log[] = ['Апдейт удался', $qs_update_relation];
+            } else {
+                $log[] = 'Апдейт не удался';
+            }
+        } else {
+            $log[] = ['Такого атрибута нет, создаем запись'];
+            $mysqli->query("INSERT INTO attr_prod_relation (attribute, attribute_value, product) VALUES ($attribute_id, $attribute_value_id, $product_id)");
+        }
     }
 }
 
@@ -46,18 +70,9 @@ foreach ($_FILES as $not_named_variable_name => $file) {
     }
 }
 
-if (isset($_POST['attributes'])) {
-    foreach ($_POST['attributes'] as $attribute_id => $attribute_value_id) {
-        $qs = "SELECT * FROM attr_prod_relation WHERE attribute = $attribute_id AND attribute_value = $attribute_value_id AND product = $product_id";
-        $res = $mysqli->query($qs)->fetch_assoc();
-        if ($res) {
-            $mysqli->query("UPDATE attr_prod_relation SET attribute_value = $attribute_value_id WHERE id = " . $res['id']);
-        } else {
-            $mysqli->query("INSERT INTO attr_prod_relation (attribute, attribute_value, product) VALUES ($attribute_id, $attribute_value_id, $product_id)");
-        }
-    }
-}
-
 echo json_encode([
     'success' => true,
+    // 'data' => [
+    //     'log' => $log,
+    // ]
 ]);
