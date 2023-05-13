adb6fefc9ac6720b13aa23d9307a859dc34b2668
Yeminfu <yeminfu4319@gmail.com>
2023-05-13 12:22:27 +1000

    upgrade

--- api/modules/create-category/create-category.php
+++ api/modules/create-category/create-category.php
@@ -3,13 +3,13 @@
 
 $category_name = $_POST['category_name'];
 
-if ($mysqli->query("SELECT * from categories WHERE category_name='$category_name'")->num_rows) {
-    echo json_encode([
-        'success' => false,
-        'error' => 'Категория с таким названием уже существует',
-    ], JSON_UNESCAPED_UNICODE);
-    exit();
-}
+// if ($mysqli->query("SELECT * from categories WHERE category_name='$category_name'")->num_rows) {
+//     echo json_encode([
+//         'success' => false,
+//         'error' => 'Категория с таким названием уже существует',
+//     ], JSON_UNESCAPED_UNICODE);
+//     exit();
+// }
 
 
 $description = $_POST['description'];
@@ -19,7 +19,7 @@ $files = $_FILES;
 $parent = (isset($_POST['parent'])) ? ("'" . $_POST['parent'] . "'") : null;
 
 $params = [
-    'category_name' => $category_name,
+    'category_name' => $category_name . rand(0, 15),
     'description' => $description,
 ];
 if (isset($_POST['parent'])) $params['parent'] = $_POST['parent'];
@@ -46,61 +46,42 @@ try {
 
 
 foreach ($_FILES as $not_named_variable_name => $file) {
-    $fileName = basename($file['name']);
-    if ($mysqli->query("SELECT * from media WHERE name='$fileName'")->num_rows) { //проверка на наименование файла в бд
-        echo json_encode([
-            'success' => false,
-            'error' => "Категория создана, но файл с именем '$fileName' не удалось сохранить, т.к. он уже существует",
-        ], JSON_UNESCAPED_UNICODE);
-        exit();
-    } else {
-        $uploadfile = $config['uploaddir'] . "/" . basename($file['name']);
-        if (move_uploaded_file($file['tmp_name'], $uploadfile)) {
-
-            $qs = "INSERT INTO media (type,name,product_id) VALUES ('product_image','$fileName','$product_id')";
-            $result = $mysqli->query($qs);
-        } else {
+    if (count($_FILES)) {
+        $fileName = basename($file['name']);
+        if ($mysqli->query("SELECT * from media WHERE name='$fileName'")->num_rows) { //проверка на наименование файла в бд
             echo json_encode([
                 'success' => false,
-                'error' => "Не удалось сохранить '$fileName'. Пожалуйста обратитесь в службу поддержки",
+                'error' => "Категория создана, но файл с именем '$fileName' не удалось сохранить, т.к. он уже существует",
             ], JSON_UNESCAPED_UNICODE);
+            exit();
+        } else {
+            $uploadfile = $config['uploaddir'] . "/" . basename($file['name']);
+            if (move_uploaded_file($file['tmp_name'], $uploadfile)) {
+
+                $qs = "INSERT INTO media (type,name,product_id) VALUES ('category_image','$fileName','$new_category_id')";
+                $result = $mysqli->query($qs);
+            } else {
+                echo json_encode([
+                    'success' => false,
+                    'error' => "Не удалось сохранить '$fileName'. Пожалуйста обратитесь в службу поддержки",
+                ], JSON_UNESCAPED_UNICODE);
+            }
         }
     }
 }
 
 
 
-
-//TODO добавить в бд возможность создания картинок для категорий
-
-
-// foreach ($files as $file_name => $file) {
-//     if ($mysqli->query("SELECT * from media WHERE name='$file_name'")->num_rows) { //проверка на наименование файла в бд
-//         echo json_encode([
-//             'success' => false,
-//             'error' => "Файл с именем '$file_name' уже существует",
-//         ]);
-//         exit();
-//     } else {
-//         $uploaddir = __DIR__ . '/images/';
-//         $uploadfile = $uploaddir . basename($file['name']);
-//         if (move_uploaded_file($file['tmp_name'], $uploadfile)) {
-//             $qs = "INSERT INTO media (type,name,product_id) VALUES ('product_image','$file_name','$new_product_id')";
-//             $result = $mysqli->query($qs);
-//         } else {
-//             echo json_encode([
-//                 'success' => false,
-//                 'error' => "Не удалось сохранить '$file_name'. Пожалуйста обратитесь в службу поддержки",
-//             ]);
-//         }
-//     }
-// }
-
 echo json_encode([
     'success' => true,
     'product' => [
         // 'name' => $product_name,
         'new_category_id' => $new_category_id,
+        'file' => ([
+            // $_POST['image'],
+            count($_FILES),
+            $_FILES,
+        ]),
         // 'price' => $price,
         // 'description' => $description,
         // 'images' => $mysqli->query("SELECT * FROM media WHERE product_id = '$new_product_id'")->fetch_all(MYSQLI_ASSOC),
